99b0a184f5e4aaed2f816be1466e0aed9c5fadeb
nandan <nandan.sarker13@gmail.com>
2019-10-09 00:14:37 +0600

    Employee seeding in database >> assignment_db

---
+++ database/migrations/2019_10_05_074314_create_employees_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('address');
+            $table->string('country');
+            $table->string('city');
+            $table->integer('salary');
+            $table->date('birth_date');
+            $table->string('credit_card_no');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
